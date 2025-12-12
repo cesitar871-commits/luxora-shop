937d91985af27db294ab4332561c288a370610e5
luxora-mx <cesitar871@gmail.com>
2025-12-12 01:13:44 -0600

    Add initial list of perfumes with details

---
+++ data/perfumes.js
@@ -0,0 +1,35 @@
+export const perfumes = [
+  {
+    name: "Dior Sauvage",
+    sizes: [
+      {
+        ml: "100 ml",
+        priceId: "",
+        productId: "",
+      },
+    ],
+    image: "",
+  },
+  {
+    name: "Bleu de Chanel",
+    sizes: [
+      {
+        ml: "100 ml",
+        priceId: "",
+        productId: "",
+      },
+    ],
+    image: "",
+  },
+  {
+    name: "Acqua di Gio Profumo",
+    sizes: [
+      {
+        ml: "100 ml",
+        priceId: "",
+        productId: "",
+      },
+    ],
+    image: "",
+  },
+];
